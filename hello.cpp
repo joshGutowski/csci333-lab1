60d83374b02b16fb15adcdb5ab5f6f2ba33bbac4
root <root@debian.rbh.rockynet.wireless.unca>
2012-09-06 16:45:39 -0400

    Completed Project Euler 1

--- hello.cpp
+++ hello.cpp
@@ -4,9 +4,15 @@ using std::cout;
 using std::endl;
 
 int main() {
-  cout<<"hello world!"<<endl;
+  // cout<<"hello world!"<<endl;
 
-
-
+  int sum = 0;
+  for(int i= 0; i < 1000; ++i) {
+    if(i%3 == 0 || i%5 == 0) {
+      sum = sum + i;
+    }
+  }
+  cout <<"The sum of all natural numbers below 1000 that are multiples of 3 or 5 is"<< endl;
+  cout <<sum <<endl;
   return 0;
 }
